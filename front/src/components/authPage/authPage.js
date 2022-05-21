9387536854911145860d8d6c6580d995524d35b3
AlexZenevich <58400324+AlexZenevich@users.noreply.github.com>
2022-05-21 22:26:23 +0300

    add globalstyles, edit authPage

--- front/src/components/authPage/authPage.js
+++ front/src/components/authPage/authPage.js
@@ -2,14 +2,35 @@ import React from 'react';
 import styled from "styled-components";
 
 const AuthInput = styled.input`
+height: 40px;
+width: 300px;
+margin-bottom: 50px;
+margin-left:auto;
+margin-right: auto;
+
+`
+const AuthFullPage = styled.div`
+display: flex;
+height: 100vh;`
+
+const AuthWindow = styled.div`
+background: rgb(204,204,204,1);
+width: 500px;  
+height: 400px;
+margin: auto;
+display: flex;
+flex-direction: column;
+justify-content: center;
 `
 
 const AuthPage = () => {
     return (
-        <div>
+        <AuthFullPage>
+        <AuthWindow>
 <AuthInput placeholder='Введите логин:'/>
 <AuthInput type='password' placeholder='Введите пароль:'/>
-        </div>
+        </AuthWindow>
+        </AuthFullPage>
     );
 };
 
--- front/src/index.js
+++ front/src/index.js
@@ -4,14 +4,21 @@ import {Provider} from 'react-redux'
 import store from './stateManagment/store'
 import {BrowserRouter} from "react-router-dom";
 import Routers from "./router/routes";
+import {createGlobalStyle} from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+margin: 0;
+padding: 0;
+`
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-        <Provider store={store}>
-            <Routers/>
-        </Provider>
+            <GlobalStyles/>
+            <Provider store={store}>
+                <Routers/>
+            </Provider>
         </BrowserRouter>
     </React.StrictMode>
 );
